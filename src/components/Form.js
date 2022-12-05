1353a257a7cffe0ee3149fae40ef498ccf6a7ddf
Gabriel Andrade <gabrielfootze@gmail.com>
2022-12-05 08:42:36 -0300

    formFeito

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,37 @@
 import './FormStyles.css'
-
+import emailjs from 'emailjs-com'
 import React from 'react'
 
 const Form = () => {
+
+function sendEmail(e) {
+        e.preventDefault();
+
+    emailjs.sendForm('gmailMessage', 'template_rxxi3cg', e.target, 'FWfIqkWp5xVQgLugA')
+
+        .then((result) => {
+            alert("Mensagem enviada com sucesso! 👍");
+           
+        }, (error) => {
+            alert(error.message)
+            
+        });
+        e.target.reset()
+
+ 
+    }
+
   return (
     <div className='form'>
-        <form>
+          <form onSubmit={sendEmail}>
             <label>Seu Nome</label>
-            <input type='text'></input>
+            <input type='text' required placeholder='Seu Nome' name='name'></input>
             <label>Seu Email</label>
-            <input type='email'></input>
+            <input type='email' required placeholder='Seu Email' name='email'></input>
             <label>Mensagem</label>
-            <textarea rows='6' placeholder='Escreva sua mensagem aqui'/>
+            <textarea id='' rows='6' placeholder='Envie sua mensagem aqui' name='message'/>
             <button className='btn'>Enviar</button>
-        </form>
+         </form>
     </div>
   )
 }
